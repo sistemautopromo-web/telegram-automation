d5ce1dabdf8dea5db350afb68545196eddf59708
systemautopromo-web <historiasdavidahh@gmail.com>
2026-01-04 21:06:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Automação iniciada com sucesso")
+import os
+import requests
+
+token = os.environ["TELEGRAM_TOKEN"]
+chat_id = os.environ["TELEGRAM_CHAT_ID"]
+
+mensagem = "✅ Teste automático funcionando!"
+
+url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+requests.post(url, json={
+    "chat_id": chat_id,
+    "text": mensagem
+})
